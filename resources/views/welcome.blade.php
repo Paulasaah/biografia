e848347758a2b82058a1c61f6427e572cbaf0c08
Paulasaah <paulasaavedra1101@gmail.com>
2025-09-18 00:17:19 -0500

    Lugar de nacimiento y procedencia

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,5 +8,8 @@
 </head>
 <body>
     <h1>Mi nombre es Maria Paula Saavedra</h1>
+    <h2>Mi Lugar de Nacimiento y Procedencia</h2>
+    <p>Nací en la <em>ciudad bonita</em>, Bucaramanga, el 5 de enero del 2004. Mis padres, <strong>Martha</strong> y <strong>Germán</strong>, me recibieron con amor y entusiasmo, acompañados de mi hermano <strong>Andrés Felipe</strong>, cariñosamente llamado <em>Pipe</em>. Desde pequeña crecí en un hogar lleno de cariño, valores y apoyo incondicional.</p>
+
 </body>
 </html>
